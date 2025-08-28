9014925481767e6a43a24b7ac8e7ab6c4e95500f
Matheus Sanada <maatheusdavid@me.com>
2025-08-28 17:05:32 -0300

    v0.0.1

---
+++ Casks/astrae.rb
@@ -0,0 +1,16 @@
+cask "astrae" do
+  version "0.0.1"
+  sha256 "f95a5691f903d65c6bc76786573e5733ad63a171fbfe6abce4ba0a0f9e3d3aee"
+
+  url "https://github.com/mdSanada/astrae/releases/download/v#{version}/Astrae.zip"
+  name "Astrae"
+  desc "Astrae: seu aplicativo em SwiftUI para macOS"
+  homepage "https://github.com/mdSanada/astrae"
+
+  app "Astrae.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.sanada.Astrae.plist",
+    "~/Library/Application Support/Astrae",
+  ]
+end
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# homebrew-astrae
+# Homebrew Tap para Astrae 🍺
+
+Este repositório disponibiliza o [Astrae](https://github.com/mdSanada/astrae) via **Homebrew Cask**.
+
+## Instalação
+```bash
+brew tap mdSanada/astrae
+```
+
+```bash
+brew install --cask astrae
+```
+
+## Upgrade
+```bash
+brew upgrade --cask astrae
+```
+
+## Desinstalação
+```bash
+brew uninstall --cask astrae
+```
