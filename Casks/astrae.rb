5b9c01b3b96a20698823f8ff80e7f070dbc29ab1
Matheus Sanada <maatheusdavid@me.com>
2025-08-28 17:44:25 -0300

    Update astrae.rb

--- Casks/astrae.rb
+++ Casks/astrae.rb
@@ -2,7 +2,7 @@ cask "astrae" do
   version "0.0.1"
   sha256 "f95a5691f903d65c6bc76786573e5733ad63a171fbfe6abce4ba0a0f9e3d3aee"
 
-  url "https://github.com/mdSanada/astrae/releases/download/v#{version}/Astrae.zip"
+  url "https://github.com/mdSanada/astrae/releases/download/#{version}/Astrae.zip"
   name "Astrae"
   desc "Astrae: seu aplicativo em SwiftUI para macOS"
   homepage "https://github.com/mdSanada/astrae"
